1ff1de6f13ff42959aa1f38abc80c6d5d8c9f24a
null-byt3 <null-byt3@users.noreply.github.com>
2020-12-27 17:52:52 +0200

    .

--- Hello.java
+++ Hello.java
@@ -1,9 +1,9 @@
 public class Hello {
     public static void main(String[] args) {
 
-        String[] text = "Goodbye Java! Good";
-
-        while (text.length)
+        String text = "Goodbye Java! Good Riddance";
+        
+        while (text.length() > 0) {
             System.out.println(text);
             text = text.substring(0, text.length() - 1);
         }
